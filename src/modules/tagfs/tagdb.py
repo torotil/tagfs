88fc93907bdd445cb7bbb242f02458c474597159
archaon <archaon@hell>
2010-08-25 23:06:00 +0000

    Fix add file

--- src/modules/tagfs/tagdb.py
+++ src/modules/tagfs/tagdb.py
@@ -20,7 +20,27 @@ class TagDB:
 		
 	def addFile(self, file):
 		cursor = self.connection.cursor()
+		subdirs = file.split('/')
+		whereclause = '( 1=2'
+		
+		for i in range(1, len(subdirs)-1): #element 0 is always null for well formed paths
+			path = ''
+			for j in range (1,i+1):
+				path = path + '/' + subdirs[j]
+			whereclause += '\n or f.path like \'' +  path + '%\''	
+		whereclause += ')'
+			
 		cursor.execute('INSERT INTO files(path) VALUES(\'' + file +'\')')
+		
+		cursor.execute('INSERT INTO tags(fid, tag) '\
+					   'SELECT a.id, b.tag '\
+					   'FROM files a, '\
+						'(SELECT t.tag '\
+	 					' FROM files f, tags t '\
+	 	 				' where f.path = ' + whereclause + ' '\
+	 	 	 			' AND t.fid = f.id '\
+		 	 	 		') b '\
+		 	 	 		'where a.path = \'' + file + '\'')
 		
 		self.connection.commit()
 		
