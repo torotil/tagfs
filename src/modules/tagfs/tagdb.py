9bfcb1b705a0a5c1fc2460c4f3331717e5f6e80a
archaon <archaon@hell>
2010-08-28 19:08:51 +0000

    add functions:     removeAllTagsFromDirectory     removeAllTagsFromFile     resetTagsForFileTo     resetTagsForDirectoryTo     getTagsForDirectory     getModtime     setModtime

--- src/modules/tagfs/tagdb.py
+++ src/modules/tagfs/tagdb.py
@@ -12,12 +12,30 @@ class TagDB:
 		
 		#TODO BETTER INDIZES + KEYs
 		
+		cursor.execute('CREATE TABLE IF NOT EXISTS global_metadata (modtime NUMERIC)')
+		
+		cursor.execute('SELECT count(modtime) FROM global_metadata')
+		
+		if cursor.fetchone()[0] == 0 :
+			cursor.execute('INSERT INTO global_metadata(modtime) VALUES(0)')
+		
+			
+		
 		cursor.execute('CREATE TABLE IF NOT EXISTS files (id INTEGER PRIMARY KEY, path VARCHAR UNIQUE)')
 		cursor.execute('CREATE TABLE IF NOT EXISTS tags  (tid INTEGER,fid INTEGER, tag VARCHAR)')
 		cursor.execute('CREATE TABLE IF NOT EXISTS tagvalues  (tid INTEGER,value VARCHAR)')
 		
 		self.connection.commit()
 		
+	def getModtime(self):
+		cursor = self.connection.cursor()
+		cursor.execute('SELECT modtime FROM global_metadata')
+		return cursor.fetchone()[0]
+	
+	def setModtime(self,timestamp):
+		cursor = self.connection.cursor()
+		cursor.execute('UPDATE global_metadata set modtime = ' + str(timestamp))
+		self.connection.commit()
 		
 	def addFile(self, file):
 		cursor = self.connection.cursor()
@@ -75,15 +93,39 @@ class TagDB:
 		
 	def removeTagFromDirectory(self,tag,path):
 		cursor = self.connection.cursor()
-		cursor.execute('DELETE FROM tags '\
-					   'WHERE fid in (SELECT id FROM FILES WHERE path like \''+path+'%\')')
+		cursor.execute(' DELETE FROM tags '\
+					   ' WHERE fid in (SELECT id FROM FILES WHERE path like \''+path+'%\') '\
+					   ' AND tag = \''+tag+'\'')
 		self.connection.commit()
 		
 	def removeTagFromFile(self,tag,file):
 		cursor = self.connection.cursor()
-		cursor.execute('DELETE FROM tags '\
-					   'WHERE fid = (SELECT id FROM FILES WHERE path = \''+file+'\')')
+		cursor.execute(' DELETE FROM tags '\
+					   ' WHERE fid = (SELECT id FROM FILES WHERE path = \''+file+'\') '\
+					   ' AND tag = \''+ tag + '\'' )
 		self.connection.commit()
+		
+	def removeAllTagsFromDirectory(self,path):
+		cursor = self.connection.cursor()
+		cursor.execute('DELETE FROM tags '\
+					   'WHERE fid in (SELECT id FROM FILES WHERE path like \''+path+'%\')')
+		self.connection.commit()
+		
+	def removeAllTagsFromFile(self,file):
+		cursor = self.connection.cursor()
+		cursor.execute(' DELETE FROM tags '\
+					   ' WHERE fid in (SELECT id FROM FILES WHERE path like \''+file+'%\')')
+		self.connection.commit()
+		
+	def resetTagsForDirectoryTo(self,path,taglist):
+		self.removeAllTagsFromDirectory(path)
+		for tag in taglist:
+			self.addTagToDirectory(tag, path)
+			
+	def resetTagsForFileTo(self,file,taglist):
+		self.removeAllTagsFromFile(file)
+		for tag in taglist:
+			self.addTagToFile(tag, file)
 		
 	def getTagsForFile(self, file):
 		cursor = self.connection.cursor()
@@ -91,6 +133,19 @@ class TagDB:
 		cursor.execute('SELECT b.tag FROM '\
 					   'files a, tags b '\
 					   'WHERE a.path = \'' + file + '\''\
+					   'AND b.fid = a.id')
+		
+		for row in cursor:
+			ret.append(row[0])
+		
+		return ret
+	
+	def getTagsForDirectory(self, path):
+		cursor = self.connection.cursor()
+		ret = []
+		cursor.execute('SELECT b.tag FROM '\
+					   'files a, tags b '\
+					   'WHERE a.path like \'' + path + '%\''\
 					   'AND b.fid = a.id')
 		
 		for row in cursor:
