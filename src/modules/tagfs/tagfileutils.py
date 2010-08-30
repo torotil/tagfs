cb32274c78f4b4526ea71b61bdedec4927bcd5b4
georg wiltschek <georg.wiltschek@gmail.com>
2010-08-30 15:37:49 +0200

    TagFileUtils.parseTagfile(tagfile) to read a .tag file and update the database accordingly. already in use by notifyd.py

--- src/modules/tagfs/tagfileutils.py
+++ src/modules/tagfs/tagfileutils.py
@@ -1,28 +1,82 @@
 #!/usr/bin/python
 
-debug = True
+import os
+from tagdb import *
 
-class TagFileUtils
+debug = True
+tagfsroot = os.path.abspath("/tmp/tagfs")
+
+#connect to the database
+homedir = os.path.expanduser('~')
+db = TagDB(homedir + "/.tagfs/db.sqlite")
+
+class TagFileUtils:
+
+	def mkpath(self, path):
+		prefix_len = len(tagfsroot)
+		newpath = path[prefix_len:]
+		if newpath == "": newpath = "/" 
+		if debug: print "original path: " + path
+		if debug: print "truncated path: " + newpath
+		return newpath
 
 	# appends a number of tags to the tagfiles directory section
-	def appendDirectoryTags(tagfile, taglist)
+	def appendDirectoryTags(self, tagfile, taglist):
 		return
 
 	# appends a number of tags to a file
-	def appendFileTags(tagfile, filetotag, taglist)
+	def appendFileTags(self, tagfile, filetotag, taglist):
 		return
 
 	# removes a file and it's tag from the tagfile
-	def removeFileFromTagfile(tagfile)
+	def removeFileFromTagfile(self, tagfile):
 		return
 
 	# removes one or more tags from a file entry
-	def removeFileTagsFromTagfile(tagfile, file, taglist)
+	def removeFileTagsFromTagfile(self, tagfile, file, taglist):
 		return
 	
 	# removes one or more tags from the directory section of a tagfile
-	def removeDirectoryTags(tagfile, taglist)
+	def removeDirectoryTags(self, tagfile, taglist):
+		return
 
 	# parses a tagfile and updates the db
-	def parseTagfile(tagfile, db)
-		return
+	def parseTagfile(self, tagfile):
+		tags = []
+		directoryTags = []
+		fileTags = []
+		pos = 0
+
+		f = open(tagfile, 'r')
+		for line in f:
+			tags.append(line)
+		f.close()
+
+		size = len(tags)
+		
+		if size == 0: return
+
+		while not tags[pos].startswith("[\"") and pos < size-1:
+			if tags[pos] != "\n": directoryTags.append(tags[pos])
+			pos+=1
+
+		if debug: print "directory tags: ", directoryTags
+		db.resetTagsForDirectoryTo(self.mkpath(tagfile[:-5]), directoryTags)
+		
+		# extract filename
+		while pos < size-1:
+			fileTags = []
+			filename = tags[pos][2:-3]
+			pos+=1
+			if tags[pos] != "\n": fileTags.append(tags[pos])
+			
+			while not tags[pos].startswith("[\"") and pos < size-1:
+				pos+=1
+				if tags[pos] != "\n" and not tags[pos].startswith("[\""):
+					fileTags.append(tags[pos])
+
+			db.removeAllTagsFromFile(self.mkpath(tagfile[:-4] + filename))
+			for tag in fileTags:
+				db.addTagToFile(tag, self.mkpath(tagfile[:-4] + filename))
+			if debug: print "filetags for file " + filename + " : ", fileTags
+
