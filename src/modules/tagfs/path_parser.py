08f06e0db9605892620cc94b00bae6ce68ce6022
Roman Zimmermann <torotil@gmail.com>
2010-08-30 19:49:12 +0200

    detect "duplicate" workaround in paths

--- src/modules/tagfs/path_parser.py
+++ src/modules/tagfs/path_parser.py
@@ -16,17 +16,27 @@ class PathParser:
 		pass
 	
 	def parse(self, path):
-		return [split_list('AND', p) for p in split_list('OR', path.split('/')[1:])]
-	
-	def get_source_file(self, path):
-		parts = self.parse(path)
-		last = parts[-1][-1]
+		ret = [split_list('AND', p) for p in split_list('OR', path.split('/')[1:])]
+		last = ret[-1][-1]
 		if len(last) > 1:
 			filename = last[-1]
-			# TODO: exrtact path from optional "(in /path/in/itemsDir)"
-		parts[-1][-1] = [last[0]]
+			if filename[-1] == ')':
+				r = filename.rfind(' (in ')
+				if r > 0:
+					path = filename[r+5:-1]
+					filename = filename[:r]
+		else:
+			filename = None
+			path = None
+		ret = [[y[0] for y in x] for x in ret]
+		return (ret, path, filename)
+	
+	
+	def get_source_file(self, path):
+		parts, path, filename = self.parse(path)
+		# TODO: exrtact path from optional "(in /path/in/itemsDir)"
 		q = '(SELECT fid, count(*) FROM tags WHERE tag IN(%s) GROUP BY fid HAVING count(*)=%d)'
-		query = ' UNION '.join([q % ("'"+"', '".join([y[0] for y in x])+"'", len(x)) for x in parts])
+		query = ' UNION '.join([q % ("'"+"', '".join(x)+"'", len(x)) for x in parts])
 		return query
 		
 			
