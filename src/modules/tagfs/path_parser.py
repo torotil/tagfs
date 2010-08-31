9bc1979af8e4f060466dfa8a6ab8a69c079499b9
archaon <johannesspiessberger@yahoo.de>
2010-08-31 00:48:44 +0000

    fix parser + add last missing methods to db

--- src/modules/tagfs/path_parser.py
+++ src/modules/tagfs/path_parser.py
@@ -33,9 +33,13 @@ class PathParser:
 	
 	
 	def get_source_file(self, path):
+		
+		if path == '/':
+			return ' SELECT id as fid FROM items '
+		
 		parts, path, filename = self.parse(path)
 		# TODO: exrtact path from optional "(in /path/in/itemsDir)"
-		q = ' SELECT fid, count(*) FROM tags WHERE tag IN(%s) GROUP BY fid HAVING count(*)=%d '
+		q = ' SELECT a.id as fid, count(*) FROM (SELECT a.id, b.pid FROM items a LEFT JOIN hierarchy b ON (a.id = b.cid)) a, tags b WHERE b.tag IN(%s) AND (b.fid = a.id OR b.fid = a.pid) GROUP BY a.id HAVING count(*)=%d '
 		query = ' UNION '.join([q % ("'"+"', '".join(x)+"'", len(x)) for x in parts])
 		return query
 		
